badd85ea60887d926ffe2b60e0f9546f0754bcf1
DarinaKir <dasha23kir@gmail.com>
2024-09-03 11:33:42 +0300

    03.09.2024

---
+++ src/main/java/com/ashcollege/entities/OutfitSuggestion.java
@@ -0,0 +1,38 @@
+package com.ashcollege.entities;
+
+import java.util.List;
+
+public class OutfitSuggestion {
+    private List<OutfitItem> outfit;
+    private String explanation;
+
+    public OutfitSuggestion(List<OutfitItem> outfit, String explanation) {
+        this.outfit = outfit;
+        this.explanation = explanation;
+    }
+
+    public List<OutfitItem> getOutfit() {
+        return outfit;
+    }
+
+    public void setOutfits(List<OutfitItem> outfit) {
+        this.outfit = outfit;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+
+    @Override
+    public String toString() {
+        return "OutfitSuggestion{" +
+                "outfit=" + outfit +
+                ", explanation='" + explanation + '\'' +
+                '}';
+    }
+}
